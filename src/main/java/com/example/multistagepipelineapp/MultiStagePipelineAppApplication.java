160ad04269023995f220d26a4f419edc69ad2ecd
Zvi Grinberg <zgrinber@redhat.com>
2022-11-18 15:44:22 +0200

    initial multistage dockerfile pipeline

---
+++ src/main/java/com/example/multistagepipelineapp/MultiStagePipelineAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.multistagepipelineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiStagePipelineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiStagePipelineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multistagepipelineapp/MultiStagePipelineAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.multistagepipelineapp;
+
+import graphql.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class MultiStagePipelineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assert.assertTrue(true);
+
+	}
+
+}
